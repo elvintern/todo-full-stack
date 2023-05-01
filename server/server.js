5d3375e38be9e499b6fd8d9b89630d79e4f4293f
elvintern <elvintern@gmail.com>
2023-05-01 20:31:38 +1200

    Deploy todo-full-stack

--- server/server.js
+++ server/server.js
@@ -8,7 +8,11 @@ server.use(express.static(path.join(__dirname, '../public')))
 server.use('/api/v1/todos', todos)
 
 server.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'))
+  if (process.env.NODE_ENV === 'production') {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+  } else {
+    res.sendFile(path.join(__dirname, '..', 'index.html'))
+  }
 })
 
 module.exports = server
