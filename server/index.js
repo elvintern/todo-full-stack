94e108dd13c331158b90221cfd1bf057e20ca316
elvintern <elvintern@gmail.com>
2023-05-01 20:22:00 +1200

    Add Host

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const server = require('./server')
+const HOST = process.env.HOST || '0.0.0.0'
 
 const PORT = process.env.PORT || 3000
 
-server.listen(PORT, () => {
+server.listen(PORT, HOST, () => {
   // eslint-disable-next-line no-console
-  console.log('Listening on port', PORT)
+  console.log('Listening on port', `${HOST}:${PORT}`)
 })
